ea72f8d5327f5d9510ff28c12af116efd5ab92ed
jared-heidt <jared.heidt14@gmail.com>
2022-10-31 14:59:16 -0400

    Created ParkingTicketTest.java and testToString()

---
+++ app/src/test/java/alphateam/ParkingTicketTest.java
@@ -0,0 +1,38 @@
+package alphateam;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ParkingTicketTest {
+    ParkingTicket parkingTicket;
+    Car truck;
+
+    @Before
+    public void setUp() {
+        // TODO apply car refactor
+        truck = new Car();
+        truck.CarColor = "Red";
+	    truck.NumberPlate = "ZZT-0420";
+		truck.CarType = "Pickup Truck";
+
+        parkingTicket = new ParkingTicket("11/27/2020", "05:35:00", 1337, "Credit", 14, truck);
+    }
+
+    @Test 
+    public void testToString() throws Exception {
+        String expectedStr = "----- Parking Ticket -----" +
+        "\nCar Number: " + parkingTicket.getAssignedCar().getNumberPlate() +
+        "\nCar Color: " + parkingTicket.getAssignedCar().getCarColor() + 
+        "\nCar Type: " + parkingTicket.getAssignedCar().getCarType() +
+        "\nTime: " + parkingTicket.getEnterTime() + 
+        "\nDate: " +  parkingTicket.getEnterDate() +
+        "\nSpot Number: " + parkingTicket.getSpotNumber();
+
+        String actualStr = parkingTicket.toString();
+        assertEquals(expectedStr, actualStr);
+    }
+}
